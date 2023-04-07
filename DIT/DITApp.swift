b7e2f79eecfc194fd95a3b045188445401f87622
cdann <chris@dannunzio.net>
2023-04-06 18:38:35 -0700

    Initial Commit

---
+++ DIT/DITApp.swift
@@ -0,0 +1,17 @@
+//
+//  DITApp.swift
+//  DIT
+//
+//  Created by cdann on 4/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
